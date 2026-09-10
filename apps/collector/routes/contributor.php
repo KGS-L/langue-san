80b1f9a58890ebab914a47e3a859bb83b9cd6e63
SO Kevin Jonas Gningnabe <142751744+sokevinjonas@users.noreply.github.com>
2026-09-10 21:54:38 +0000

    feat: route contributor context step

--- apps/collector/routes/contributor.php
+++ apps/collector/routes/contributor.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Contributor\ContributorContextController;
 use App\Http\Controllers\Contributor\ContributorHomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/contribute', ContributorHomeController::class)->name('contributor.home');
+Route::get('/contribute/context', [ContributorContextController::class, 'edit'])->name('contributor.context.edit');
+Route::post('/contribute/context', [ContributorContextController::class, 'update'])->name('contributor.context.update');
