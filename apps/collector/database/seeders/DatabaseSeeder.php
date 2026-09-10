c1c0544ab299255e0d1e35570b155969a2b52c8e
SO Kevin Jonas Gningnabe <142751744+sokevinjonas@users.noreply.github.com>
2026-09-10 21:39:11 +0000

    feat: seed initial prompt catalog by default

--- apps/collector/database/seeders/DatabaseSeeder.php
+++ apps/collector/database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             LinguisticReferenceSeeder::class,
             CategorySeeder::class,
+            PromptSeeder::class,
             AdminUserSeeder::class,
         ]);
     }
