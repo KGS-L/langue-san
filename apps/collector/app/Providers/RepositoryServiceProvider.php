44fbaedfdcf7a0c962156a731991a0f21ab0aad9
SO Kevin Jonas Gningnabe <142751744+sokevinjonas@users.noreply.github.com>
2026-09-11 03:21:55 +0000

    feat: bind community repositories

--- apps/collector/app/Providers/RepositoryServiceProvider.php
+++ apps/collector/app/Providers/RepositoryServiceProvider.php
@@ -8,8 +8,11 @@ use App\Contracts\Repositories\ContributionRepositoryInterface;
 use App\Contracts\Repositories\ContributorConsentRepositoryInterface;
 use App\Contracts\Repositories\ContributorProfileRepositoryInterface;
 use App\Contracts\Repositories\LocalityRepositoryInterface;
+use App\Contracts\Repositories\ProjectApplicationRepositoryInterface;
+use App\Contracts\Repositories\ProjectMembershipRepositoryInterface;
 use App\Contracts\Repositories\PromptRepositoryInterface;
 use App\Contracts\Repositories\RecordingRepositoryInterface;
+use App\Contracts\Repositories\UserProfileRepositoryInterface;
 use App\Contracts\Repositories\UserRepositoryInterface;
 use App\Contracts\Repositories\ValidationRepositoryInterface;
 use App\Contracts\Repositories\VarietyRepositoryInterface;
@@ -19,8 +22,11 @@ use App\Repositories\Eloquent\ContributionRepository;
 use App\Repositories\Eloquent\ContributorConsentRepository;
 use App\Repositories\Eloquent\ContributorProfileRepository;
 use App\Repositories\Eloquent\LocalityRepository;
+use App\Repositories\Eloquent\ProjectApplicationRepository;
+use App\Repositories\Eloquent\ProjectMembershipRepository;
 use App\Repositories\Eloquent\PromptRepository;
 use App\Repositories\Eloquent\RecordingRepository;
+use App\Repositories\Eloquent\UserProfileRepository;
 use App\Repositories\Eloquent\UserRepository;
 use App\Repositories\Eloquent\ValidationRepository;
 use App\Repositories\Eloquent\VarietyRepository;
@@ -30,6 +36,9 @@ class RepositoryServiceProvider extends ServiceProvider
 {
     public array $bindings = [
         UserRepositoryInterface::class => UserRepository::class,
+        UserProfileRepositoryInterface::class => UserProfileRepository::class,
+        ProjectApplicationRepositoryInterface::class => ProjectApplicationRepository::class,
+        ProjectMembershipRepositoryInterface::class => ProjectMembershipRepository::class,
         ContributorProfileRepositoryInterface::class => ContributorProfileRepository::class,
         ContributorConsentRepositoryInterface::class => ContributorConsentRepository::class,
         CategoryRepositoryInterface::class => CategoryRepository::class,
