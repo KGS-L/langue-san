dec86f77a78ab67c31b1136e7eb85901e678d6db
SO Kevin Jonas Gningnabe <142751744+sokevinjonas@users.noreply.github.com>
2026-09-11 04:11:01 +0000

    fix: keep demo contributors out of default seed

--- apps/collector/database/seeders/DatabaseSeeder.php
+++ apps/collector/database/seeders/DatabaseSeeder.php
@@ -2,13 +2,6 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\AdminUserSeeder;
-use Database\Seeders\CategorySeeder;
-use Database\Seeders\ConsentVersionSeeder;
-use Database\Seeders\DemoContributorSeeder;
-use Database\Seeders\LinguisticReferenceSeeder;
-use Database\Seeders\PromptSeeder;
-use Database\Seeders\RolePermissionSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -22,7 +15,6 @@ class DatabaseSeeder extends Seeder
             ConsentVersionSeeder::class,
             RolePermissionSeeder::class,
             AdminUserSeeder::class,
-            DemoContributorSeeder::class,
         ]);
     }
 }
