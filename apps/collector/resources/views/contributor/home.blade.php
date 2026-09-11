3f70ca1f4d61530a997519359e511e2918e3c72e
SO Kevin Jonas Gningnabe <142751744+sokevinjonas@users.noreply.github.com>
2026-09-11 04:21:35 +0000

    style: align contributor home buttons with public branding

--- apps/collector/resources/views/contributor/home.blade.php
+++ apps/collector/resources/views/contributor/home.blade.php
@@ -7,6 +7,7 @@
     <link rel="icon" type="image/svg+xml" href="{{ asset('assets/img/langue-san-logo.svg') }}">
     <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/public-theme.css') }}" rel="stylesheet">
 </head>
 <body class="bg-light">
 <nav class="navbar bg-white border-bottom">
